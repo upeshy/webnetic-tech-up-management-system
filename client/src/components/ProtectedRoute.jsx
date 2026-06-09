a353ad3d53b9ef9161f56775e1c8dd23ff6d2aaa
upeshy <yadavupesh39@gmail.com>
2026-06-09 19:50:20 +0530

    Update ProtectedRoute.jsx

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,4 +1,13 @@
+import { Navigate } from 'react-router-dom';
+
 const ProtectedRoute = ({ children }) => {
+
+  const token = localStorage.getItem("token");
+
+  if (!token) {
+    return <Navigate to="/login" replace />;
+  }
+
   return children;
 };
 
